a8a059bf9a5f90751c061b09fe59b13bb8d76e06
JeanExtreme002 <jeangamerextreme@gmail.com>
2021-10-21 20:12:36 -0300

    fixed tabs

--- java/Main.java
+++ java/Main.java
@@ -1,45 +1,45 @@
 public class Main {
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		if (args.length == 0) {
-			throw new ArrayIndexOutOfBoundsException("Specify the number of digits to be calculated");
-		}
+        if (args.length == 0) {
+            throw new ArrayIndexOutOfBoundsException("Specify the number of digits to be calculated");
+        }
 
-		int exponent = Integer.parseInt(args[0]);
+        int exponent = Integer.parseInt(args[0]);
 
-		if (exponent <= 0) {
-			throw new IllegalArgumentException("The number of digits must be greater than zero");
-		}
+        if (exponent <= 0) {
+            throw new IllegalArgumentException("The number of digits must be greater than zero");
+        }
 
-		double weight = Math.pow(100, exponent - 1);
-		Block block1 = new Block(1, 0, 50);
-		Block block2 = new Block(weight, -1.0, 100);
+        double weight = Math.pow(100, exponent - 1);
+        Block block1 = new Block(1, 0, 50);
+        Block block2 = new Block(weight, -1.0, 100);
 
-		while (block1.getCollisionCount() == 0 || block1.canCollide(block2)) {
-			block1.move();
-			block2.move();
+        while (block1.getCollisionCount() == 0 || block1.canCollide(block2)) {
+            block1.move();
+            block2.move();
 
-			// Caso tenha colidido com a parede, seu sentido será invertido.
-			if (block1.getPosition() <= 0) {
-				block1.increaseCollisionCount();
-				block1.setPosition(0);
-				block1.setVelocity(block1.getVelocity() * -1);
-			}
+            // Caso tenha colidido com a parede, seu sentido será invertido.
+            if (block1.getPosition() <= 0) {
+                block1.increaseCollisionCount();
+                block1.setPosition(0);
+                block1.setVelocity(block1.getVelocity() * -1);
+            }
 
-			// Caso tenha colidido com o outro bloco, a nova velocidade de
-			// ambos os blocos será calculada.
-			if (block1.getPosition() >= block2.getPosition()) {
-				block1.setPosition(block2.getPosition());
+            // Caso tenha colidido com o outro bloco, a nova velocidade de
+            // ambos os blocos será calculada.
+            if (block1.getPosition() >= block2.getPosition()) {
+                block1.setPosition(block2.getPosition());
 
-				double v1 = Block.calculateNextVelocity(block1, block2);
-				double v2 = Block.calculateNextVelocity(block2, block1);
+                double v1 = Block.calculateNextVelocity(block1, block2);
+                double v2 = Block.calculateNextVelocity(block2, block1);
 
-				block1.setVelocity(v1);
-				block2.setVelocity(v2);
+                block1.setVelocity(v1);
+                block2.setVelocity(v2);
 
-				block1.increaseCollisionCount();
-			}
-		}
-		System.out.println("Digits of PI: " + block1.getCollisionCount());
-	}
+                block1.increaseCollisionCount();
+            }
+        }
+        System.out.println("Digits of PI: " + block1.getCollisionCount());
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,56 +1,56 @@
 function drawObjects(context, block1, block2) {
-	context.fillStyle = "#f00";
-	context.fillRect(block2.position, context.canvas.height - block2.size, block2.size, block2.size);
+    context.fillStyle = "#f00";
+    context.fillRect(block2.position, context.canvas.height - block2.size, block2.size, block2.size);
 
-	context.fillStyle = "#00f";
-	context.fillRect(block1.position, context.canvas.height - block1.size, block1.size, block1.size);
+    context.fillStyle = "#00f";
+    context.fillRect(block1.position, context.canvas.height - block1.size, block1.size, block1.size);
 }
 
 function drawData(context, block1, block2) {
-	context.fillStyle = "#fff";
-	context.font = "20px Arial";
+    context.fillStyle = "#fff";
+    context.font = "20px Arial";
 
-	const pi = "Digits of PI: " + block1.collisionCount;
-	const v1 = "V1: " + block1.velocity.toFixed(2);
-	const v2 = "V2: " + block2.velocity.toFixed(2);
+    const pi = "Digits of PI: " + block1.collisionCount;
+    const v1 = "V1: " + block1.velocity.toFixed(2);
+    const v2 = "V2: " + block2.velocity.toFixed(2);
 
-	context.fillText(`${pi}      |      ${v1}      ${v2}`, 50, 50);
+    context.fillText(`${pi}      |      ${v1}      ${v2}`, 50, 50);
 }
 
 function startAnimation(context, block1, block2, timeSteps) {
-	for (let i = 0; i < timeSteps; i++) {
-		move(block1, block2);
-		updateScreen(context, block1, block2);
-	}
-	requestAnimationFrame(() => {startAnimation(context, block1, block2, timeSteps)});
+    for (let i = 0; i < timeSteps; i++) {
+        move(block1, block2);
+        updateScreen(context, block1, block2);
+    }
+    requestAnimationFrame(() => {startAnimation(context, block1, block2, timeSteps)});
 }
 
 function move(block1, block2) {
-	block1.move();
-	block2.move();
+    block1.move();
+    block2.move();
 
-	// Se o bloco colidiu com a parede, seu sentido é invertido.
-	if (block1.position <= 0) {
-		block1.position = 0;
-		block1.velocity *= -1;
-		block1.collisionCount += 1;
-	}
+    // Se o bloco colidiu com a parede, seu sentido é invertido.
+    if (block1.position <= 0) {
+        block1.position = 0;
+        block1.velocity *= -1;
+        block1.collisionCount += 1;
+    }
 
-	// Se o bloco colidir com o outro bloco, suas novas velocidades serão calculadas.
-	if (block1.position + block1.size >= block2.position) {
-		block1.position = block2.position - block1.size;
+    // Se o bloco colidir com o outro bloco, suas novas velocidades serão calculadas.
+    if (block1.position + block1.size >= block2.position) {
+        block1.position = block2.position - block1.size;
 
-		const v1 = Block.calculateNextVelocity(block1, block2);
-		const v2 = Block.calculateNextVelocity(block2, block1);
+        const v1 = Block.calculateNextVelocity(block1, block2);
+        const v2 = Block.calculateNextVelocity(block2, block1);
 
-		block1.velocity = v1;
-		block2.velocity = v2;
-		block1.collisionCount += 1;
-	}
+        block1.velocity = v1;
+        block2.velocity = v2;
+        block1.collisionCount += 1;
+    }
 }
 
 function updateScreen(context, block1, block2) {
-	context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-	drawData(context, block1, block2);
-	drawObjects(context, block1, block2);
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    drawData(context, block1, block2);
+    drawObjects(context, block1, block2);
 }
